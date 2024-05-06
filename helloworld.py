4ebbec587e3e23adacca5fea33c1088a397b33cb
KOMMI DEEKSHITA <125177270+deekshitakommi@users.noreply.github.com>
2024-05-06 19:34:16 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,6 @@
 print("Hello world")
 
-print("Update: This update will reflect once Jenkins auto builds after 5 minutes.")
+print("Update: This update will reflect once Jenkins auto builds after few minutes.")
 
 print("Update 2: Testing once more, just to be sure.")
 
